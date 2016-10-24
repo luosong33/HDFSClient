2af45852d28f46a838790b3d0abfe743031a2207
luosong <luosong3345@gmail.com>
2016-10-24 15:48:27 +0800

    是

---
+++ README.md
@@ -0,0 +1,4 @@
+运行本地代码，操作远程hdfs
+1、编译windows版hadoop，并配置环境变量
+2、复制hadoop配置到resource
+3、加入参数    -DHADOOP_USER_NAME=root    运行即可（用hadoop插件的运行方式也行）
--- src/main/java/cn/newtouch/hadoopTest/HDFSClient.java
+++ src/main/java/cn/newtouch/hadoopTest/HDFSClient.java
@@ -13,43 +13,46 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 
 public class HDFSClient {
-	
+
 	private static Configuration config = null;
-	//private static Configuration config=new Configuration();
-    static {
-      config = HBaseConfiguration.create();// 配置
-       // config.set("fs.defaultFS", "hdfs://mycluster");
-        config.set("hbase.zookeeper.quorum", "node1:2181,node2:2181,node3:2181");//在hosts文件配置好
-    }
+
+	// private static Configuration config=new Configuration();
+	static {
+		config = HBaseConfiguration.create(); // 配置
+		// config.set("fs.defaultFS", "hdfs://mycluster");
+		config.set("hbase.zookeeper.quorum", "node1:2181,node2:2181,node3:2181"); // 在hosts文件配置好
+	}
+
 	public static void main(String[] args) throws IOException, InterruptedException, URISyntaxException {
 
-    	/*config.set("fs.defaultFS", "hdfs://10.133.250.70:9000");
-    	config.set("dfs.replication", "3");*/
-    	
-    	Iterator<Entry<String, String>> it= config.iterator();
-    	while(it.hasNext())
-    	{
-    		Entry<String, String> ent = it.next();
-    	    System.out.println(ent.getKey()+" : "+ent.getValue());
-    	}
-		 FileSystem fs = FileSystem.get(config);
-		 //FileSystem fs = FileSystem.get(new URI("hdfs://10.133.250.70:9000"), config, "root");
-         
-		 
-		 Path dst = new Path("/lwTest.txt");         
-         Path src=new Path("c:/test.txt");//c:\\test.txt也可以
-         //Thread.sleep(500000);
+		/*
+		 * config.set("fs.defaultFS", "hdfs://10.133.250.70:9000");
+		 * config.set("dfs.replication", "3");
+		 */
 
-         //FSDataOutputStream outputStream=fs.create(dst);
-         //fs.create(dst);//创建文件
-         //fs.mkdirs(dst);//新建文件夹
-         fs.copyFromLocalFile(src, dst);
-         System.out.println("Upload to"+config.get("fs.default.name"));
-         
-         FileStatus files[]=fs.listStatus(dst);
-         for(FileStatus file:files){
-             System.out.println(file.getPath());
-         }
-         fs.close();
+		Iterator<Entry<String, String>> it = config.iterator();
+		while (it.hasNext()) {
+			Entry<String, String> ent = it.next();
+			System.out.println(ent.getKey() + " : " + ent.getValue());
+		}
+		FileSystem fs = FileSystem.get(config);
+		// FileSystem fs = FileSystem.get(new URI("hdfs://10.133.250.70:9000"),
+		// config, "root");
+
+		Path dst = new Path("/ls/lsTest123.txt");
+		Path src = new Path("c:/test.txt"); // c:\\test.txt也可以
+		// Thread.sleep(500000);
+
+		// FSDataOutputStream outputStream=fs.create(dst);
+		// fs.create(dst);//创建文件
+		// fs.mkdirs(dst);//新建文件夹
+		fs.copyFromLocalFile(src, dst);
+		System.out.println("Upload to" + config.get("fs.default.name"));
+
+		FileStatus files[] = fs.listStatus(dst);
+		for (FileStatus file : files) {
+			System.out.println(file.getPath());
+		}
+		fs.close();
 	}
 }
